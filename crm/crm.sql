25d0fc86a12cf7b8599510fd231eb58cfca00a2b
pietro-franzero-kellogg <pietro.franzero@kellogg.northwestern.edu>
2022-04-14 02:23:44 +0000

    Adding solutions to crm and tacostagram

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,42 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS salespeople;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS clients;
+DROP TABLE IF EXISTS activities;
+
+
 -- CREATE TABLES
+CREATE TABLE salespeople (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE clients (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_id INTEGER,
+  first_name TEXT,
+  last_name TEXT,
+  job_title TEXT,
+  email TEXT,
+  phone TEXT
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  salesperson_id INTEGER,
+  client_id INTEGER,
+  contact_type TEXT,
+  contact_time DATETIME,
+  notes TEXT
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,56 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS follows;
 
+
+-- CREATE TABLES
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  username TEXT,
+  email TEXT,
+  phone TEXT,
+  location TEXT,
+  created_at DATETIME,
+  updated_at DATETIME
+);
+
+CREATE TABLE posts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  image_url TEXT,
+  description TEXT,
+  phone TEXT,
+  created_at DATETIME,
+  updated_at DATETIME
+);
+
+CREATE TABLE likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  post_id INTEGER,
+  liking_user_id INTEGER,
+  is_active BOOLEAN,
+  created_at DATETIME,
+  updated_at DATETIME
+);
+
+CREATE TABLE comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  post_id INTEGER,
+  commenter_id INTEGER,
+  comment TEXT,
+  created_at DATETIME,
+  updated_at DATETIME
+);
+
+CREATE TABLE follows (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  follower_id INTEGER,
+  followed_id INTEGER,
+  is_active BOOLEAN,
+  created_at DATETIME,
+  updated_at DATETIME
+);
